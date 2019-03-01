1df23fe551e7f556f9070515d7a18e42da7bdcbb
masternovab <46070544+masternovab@users.noreply.github.com>
2019-03-01 11:01:46 +0500

    Create arghelp.py

---
+++ MasterNova/utils/arghelp.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+import discord
+import discord.utils
+import json
+import inspect
+
+async def send(bot, ctx):
+        msg = ""
+        try:
+            perms = ctx.command.checks[0]
+        except:
+            perms = None
+        for x in ctx.command.params:
+            if x != "ctx":
+                if x != "self":
+                    if "=" in str(ctx.command.params[x]):
+                        msg += "[{}] ".format(x)
+                    else:
+                        msg += "<{}> ".format(x)
+        if not ctx.command.aliases:
+            aliases = "None"
+        else:
+            aliases = ", ".join([x for x in ctx.command.aliases])
+        if not perms:
+            msg = "Usage: {}{} {}\nCommand aliases: {}\nRequired permissions: None\nCommand description: {}".format(ctx.prefix, ctx.command, msg, aliases, ctx.command.help)
+        else:
+            msg = "Usage: {}{} {}\nCommand aliases: {}\nRequired permissions: {}\nCommand description: {}".format(ctx.prefix, ctx.command, msg, aliases, 
+            ", ".join(inspect.getclosurevars(perms).nonlocals["perms"]) if perms.__name__ != "is_owner_check" and str(perms).split(" ")[1].split(".")[0] != "is_main_owner" else "Bot Owner", ctx.command.help)
+        try:
+            msg += "\n\nSub commands: {}".format(", ".join([x for x in ctx.command.all_commands if x not in ctx.command.all_commands[x].aliases]))
+        except:
+            pass
+        s=discord.Embed(description=msg)
+        s.set_author(name=ctx.command, icon_url=bot.user.avatar_url)
+        return await ctx.channel.send(embed=s)
